d9bd74b10a5c5da22282201e9522155350be8da7
Anton Cherednik <Cherednik0311@gmail.com>
2024-05-22 22:46:25 +0300

    adding course project (main page ended)

---
+++ bookshelf/js/slider.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        dots: true,
+        slidesToShow: 3,
+        infinite: true,
+        speed: 500,
+        easing: 'ease',
+        touchThreshold: 10,
+        waitForAnimate: false,
+        responsive: [
+            {
+                breakpoint: 760,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 660,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+        ]
+    });
+});
